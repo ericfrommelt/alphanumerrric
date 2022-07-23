356463a4f4858df9590814273e80781c01df01e0
Eric Frommelt <ericfrommelt@gmail.com>
2022-07-23 14:03:15 -0700

    Initial commit

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <Composition001 />
+</template>
+
+<script setup>
+  useHead({
+    title: 'Alphanumerrrrrrrric',
+    viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+    charset: 'utf-8',
+    meta: [
+      { name: 'description', content: 'Abundant web' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon-96x96.png' },
+      { rel: 'stylesheet', type: 'text/css', href: 'https://cloud.typography.com/7804816/7011632/css/fonts.css'},
+    ],
+    bodyAttrs: {
+    class: 'test'
+    }
+  })
+</script>
---
+++ components/Composition001.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="grid-wrapper">
+    <div class="alpha-1">A</div>
+    <div class="alpha-2">L</div>
+    <div class="alpha-3">P</div>
+    <div class="alpha-4">H</div>
+    <div class="alpha-5">A</div>
+    <div class="alpha-6">N</div>
+    <div class="alpha-7">U</div>
+    <div class="alpha-8">M</div>
+    <div class="alpha-9">E</div>
+    <div class="alpha-10">RRRRRRRRRRRRRRR</div>
+    <div class="alpha-11">I</div>
+    <div class="alpha-12">C</div>
+  </div>
+</template>
+
+<style scoped>
+  .grid-wrapper {
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    grid-template-rows: 4;
+    height: 100%;
+    min-height: 20 rem;
+  }
+
+  .grid-wrapper > div {
+    padding: 10rem 0;
+    font-family: "Decimal A", "Decimal B";
+    font-style: normal;
+    font-weight: 400;
+  }
+
+  .alpha-1 {grid-column: 3; grid-row: 1}
+  .alpha-2 {grid-column: 4; grid-row: 1}
+  .alpha-3 {grid-column: 4; grid-row: 2}
+  .alpha-4 {grid-column: 5; grid-row: 2}
+  .alpha-5 {grid-column: 6; grid-row: 2}
+  .alpha-6 {grid-column: 7; grid-row: 2}
+  .alpha-7 {grid-column: 7; grid-row: 3}
+  .alpha-8 {grid-column: 8; grid-row: 3}
+  .alpha-9 {grid-column: 8; grid-row: 4}
+  .alpha-10 {grid-column: 9; grid-row: 4}
+  .alpha-11 {grid-column: 10; grid-row: 4}
+  .alpha-12 {grid-column: 11; grid-row: 4}
+
+</style>
